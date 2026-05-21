cc3d69f1549810aea96dbe0ab5bdca2fd7adf077
tjarbo <16938041+tjarbo@users.noreply.github.com>
2026-05-21 08:02:40 +0200

    fix: make repo specific postCreate and Start scripts optional

--- templates/pretalx-plugin/.devcontainer/post-create.sh
+++ templates/pretalx-plugin/.devcontainer/post-create.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Plugin specific post-create.sh script.
+# Add here plugin specific commands that should be executed after the container has been created.
 
 echo "Nothing to do";
 exit 0;
--- templates/pretalx-plugin/.devcontainer/post-start.sh
+++ templates/pretalx-plugin/.devcontainer/post-start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Plugin specific post-start.sh script.
+# Add here plugin specific commands that should be executed every time after the container has started.
 
 echo "Nothing to do";
 exit 0;
